1bb1c29a09acfad57cb991ffd67fd5767ac90f99
BurmAnton <a.burmatsky@gmail.com>
2023-03-27 14:25:18 +0400

    register shop_app models in admin (City, Street, Shop)

--- shops/admin.py
+++ shops/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from .models import City, Street, Shop
+
 # Register your models here.
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+@admin.register(Street)
+class StreetAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'city'
+    )
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'city',
+        'street',
+        'building',
+        'opening_time',
+        'closing_time'
+    )
